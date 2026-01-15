2e70951fe77d67f6d5e1a2a39d5b935073ed5286
Chee Bin Hoh <cheebinhoh@gmail.com>
2026-01-15 18:43:06 -0500

    .

--- test/dmn-test-dmesgnet-4.cpp
+++ test/dmn-test-dmesgnet-4.cpp
@@ -1,17 +1,15 @@
 /**
  * Copyright © 2025 Chee Bin HOH. All rights reserved.
  *
- * This test program asserts that Dmn_DMesgNet object will self
- * proclaim as the master node if no Dmn_DMesgNet object in its
- * network, and when the object is destroyed, it will relinquish
- * its master node status and inform others that it is in destroyed
- * state in its last heartbeat message.
+ * This test programs asserts that two Dmn_DMesgNet objects can
+ * one send message through a Dmn_Socket at a particular ip and port
+ * and another one receive sent message through another Dmn_Socket
+ * at the same ip and port.
  */
 
 #include <gtest/gtest.h>
 
 #include <chrono>
-#include <iostream>
 #include <memory>
 #include <string>
 #include <thread>
@@ -19,8 +17,8 @@
 
 #include "dmn-dmesgnet.hpp"
 #include "dmn-io.hpp"
+#include "dmn-pipe.hpp"
 #include "dmn-proc.hpp"
-#include "dmn-socket.hpp"
 
 #include "proto/dmn-dmesg-body.pb.h"
 #include "proto/dmn-dmesg.pb.h"
@@ -28,49 +26,141 @@
 int main(int argc, char *argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
 
-  dmn::DMesgPb dmesgpb_last{};
-  std::shared_ptr<dmn::Dmn_Io<std::string>> read_socket_2 =
-      std::make_unique<dmn::Dmn_Socket>("127.0.0.1", 5000);
-  dmn::Dmn_Proc read_proc{
-      "read_socket_2", [&read_socket_2, &dmesgpb_last]() mutable -> void {
-        while (true) {
-          auto data = read_socket_2->read();
-          if (data) {
-            dmn::DMesgPb dmesgpb_read{};
+  std::shared_ptr<dmn::Dmn_Io<std::string>> write_1 =
+      std::make_unique<dmn::Dmn_Pipe<std::string>>("write_1");
+  std::shared_ptr<dmn::Dmn_Io<std::string>> read_1 =
+      std::make_unique<dmn::Dmn_Pipe<std::string>>("read_1");
 
-            dmesgpb_read.ParseFromString(*data);
-            dmesgpb_last = dmesgpb_read;
-            std::cout << "DMesgPb: " << dmesgpb_read.ShortDebugString() << "\n";
-          } else {
+  std::shared_ptr<dmn::Dmn_Io<std::string>> write_2 =
+      std::make_unique<dmn::Dmn_Pipe<std::string>>("write_2");
+  std::shared_ptr<dmn::Dmn_Io<std::string>> read_2 =
+      std::make_unique<dmn::Dmn_Pipe<std::string>>("read_2");
+
+  auto read_from_write_1 = write_1;
+  auto write_to_read_1 = read_1;
+
+  auto read_from_write_2 = write_2;
+  auto write_to_read_2 = read_2;
+
+  dmn::DMesgPb dmesgpb1{};
+  dmn::DMesgPb dmesgpb1_body{};
+  dmn::DMesgPb dmesgpb1_sys{};
+  dmn::Dmn_Proc dmesg1_to_dmesg2{
+      "dmesg1_to_dmesg2", [read_from_write_1, write_to_read_2, &dmesgpb1,
+                           &dmesgpb1_body, &dmesgpb1_sys]() {
+        while (true) {
+          auto data = read_from_write_1->read();
+          if (!data) {
             break;
           }
+
+          dmesgpb1.ParseFromString(*data);
+          if (dmesgpb1.type() == dmn::DMesgTypePb::sys) {
+            dmesgpb1_sys = dmesgpb1;
+          } else {
+            dmesgpb1_body = dmesgpb1;
+          }
+
+          write_to_read_2->write(*data);
         }
       }};
 
-  read_proc.exec();
-  dmn::Dmn_Proc::yield();
+  dmn::DMesgPb dmesgpb2{};
+  dmn::DMesgPb dmesgpb2_body{};
+  dmn::DMesgPb dmesgpb2_sys{};
+  dmn::Dmn_Proc dmesg2_to_dmesg1{
+      "dmesg2_to_dmesg1", [read_from_write_2, write_to_read_1, &dmesgpb2,
+                           &dmesgpb2_body, &dmesgpb2_sys]() {
+        while (true) {
+          auto data = read_from_write_2->read();
+          if (!data) {
+            break;
+          }
 
-  std::shared_ptr<dmn::Dmn_Io<std::string>> write_socket_1 =
-      std::make_unique<dmn::Dmn_Socket>("127.0.0.1", 5000, true);
+          dmesgpb2.ParseFromString(*data);
+          if (dmesgpb2.type() == dmn::DMesgTypePb::sys) {
+            dmesgpb2_sys = dmesgpb2;
+          } else {
+            dmesgpb2_body = dmesgpb2;
+          }
 
-  std::shared_ptr<dmn::Dmn_Io<std::string>> read_socket_1 =
-      std::make_unique<dmn::Dmn_Socket>("127.0.0.1", 5001);
-  std::shared_ptr<dmn::Dmn_DMesgNet> dmesgnet1 =
-      std::make_unique<dmn::Dmn_DMesgNet>("dmesg1", std::move(read_socket_1),
-                                          std::move(write_socket_1));
-  read_socket_1.reset();
-  write_socket_1.reset();
-  std::this_thread::sleep_for(std::chrono::seconds(10));
-  dmesgnet1->waitForEmpty();
-  EXPECT_TRUE(!dmesgpb_last.body().sys().self().masteridentifier().empty());
-  EXPECT_TRUE(dmesgpb_last.body().sys().self().state() ==
-              dmn::DMesgStatePb::Ready);
+          write_to_read_1->write(*data);
+        }
+      }};
+
+  dmesg1_to_dmesg2.exec();
+  dmesg2_to_dmesg1.exec();
+
+  auto dmesgnet1 = std::make_unique<dmn::Dmn_DMesgNet>(
+      "dmesg1", std::move(read_1), std::move(write_1));
+
+  std::this_thread::sleep_for(std::chrono::seconds(2));
+
+  auto dmesgnet2 = std::make_unique<dmn::Dmn_DMesgNet>(
+      "dmesg2", std::move(read_2), std::move(write_2));
+
+  std::this_thread::sleep_for(std::chrono::seconds(15));
+  auto sys1 = dmesgpb1_sys.body().sys().self();
+  auto sys2 = dmesgpb2_sys.body().sys().self();
+
+  EXPECT_TRUE((sys1.state() == dmn::DMesgStatePb::Ready));
+  EXPECT_TRUE((sys2.state() == dmn::DMesgStatePb::Ready));
+  EXPECT_TRUE(("dmesg1" == sys1.masteridentifier()));
+  EXPECT_TRUE(("dmesg1" == sys2.masteridentifier()));
+  EXPECT_TRUE((1 == dmesgpb1_sys.body().sys().nodelist().size()));
+  EXPECT_TRUE(
+      ("dmesg2" == dmesgpb1_sys.body().sys().nodelist().Get(0).identifier()));
+  EXPECT_TRUE((1 == dmesgpb2_sys.body().sys().nodelist().size()));
+  EXPECT_TRUE(
+      ("dmesg1" == dmesgpb2_sys.body().sys().nodelist().Get(0).identifier()));
+
+  // send data
+  dmn::DMesgPb dmesgpb{};
+  dmesgpb.set_topic("counter sync");
+  dmesgpb.set_type(dmn::DMesgTypePb::message);
+  dmesgpb.set_sourceidentifier("writehandler");
+
+  std::string data{"Hello dmesg async"};
+  dmn::DMesgBodyPb *dmesgpb_body = dmesgpb.mutable_body();
+  dmesgpb_body->set_message(data);
+
+  auto dmesg2_read_handle =
+      dmesgnet2->openHandler("writeHandler", "counter sync");
+  EXPECT_TRUE(dmesg2_read_handle);
+
+  auto dmesg_handle = dmesgnet1->openHandler("writeHandler");
+  EXPECT_TRUE(dmesg_handle);
+  dmesg_handle->write(dmesgpb);
+
+  std::cout << "after write data from dmesgnet1\n";
+  std::this_thread::sleep_for(std::chrono::seconds(5));
+  EXPECT_TRUE((dmesgpb1_body.topic() == "counter sync"));
+
+  auto dmesgdata = dmesg2_read_handle->read();
+  std::cout << "data: " << (*dmesgdata).ShortDebugString() << "\n";
+  EXPECT_TRUE(((*dmesgdata).topic() == "counter sync"));
+
+  std::this_thread::sleep_for(std::chrono::seconds(5));
 
   dmesgnet1 = {};
-  std::this_thread::sleep_for(std::chrono::seconds(3));
-  EXPECT_TRUE(dmesgpb_last.body().sys().self().masteridentifier().empty());
-  EXPECT_TRUE(dmesgpb_last.body().sys().self().state() ==
-              dmn::DMesgStatePb::Destroyed);
+  std::cout << "after destroying 1\n";
+  std::this_thread::sleep_for(std::chrono::seconds(10));
+
+  sys2 = dmesgpb2.body().sys().self();
+
+  EXPECT_TRUE((sys2.state() == dmn::DMesgStatePb::Ready));
+  EXPECT_TRUE(("dmesg2" == sys2.masteridentifier()));
+  EXPECT_TRUE((0 == dmesgpb2.body().sys().nodelist().size()));
+
+  dmesgnet2 = {};
+  std::cout << "after destroying 2\n";
+  std::this_thread::sleep_for(std::chrono::seconds(10));
+
+  sys2 = dmesgpb2.body().sys().self();
+
+  EXPECT_TRUE((sys2.state() == dmn::DMesgStatePb::Destroyed));
+  EXPECT_TRUE(("" == sys2.masteridentifier()));
+  EXPECT_TRUE((0 == dmesgpb2.body().sys().nodelist().size()));
 
   return RUN_ALL_TESTS();
 }
