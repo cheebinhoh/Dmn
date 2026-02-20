3052ca83b609f369159026cfe59f51dddbb7d38d
Chee Bin Hoh <cheebinhoh@gmail.com>
2026-02-19 21:36:05 -0500

    only resend if there are neighbor node

--- src/dmn-dmesgnet.cpp
+++ src/dmn-dmesgnet.cpp
@@ -385,8 +385,9 @@ void Dmn_DMesgNet::createTimerProc() {
             // FIXME: maybe it is good that master resend them prioritically?
             if (m_output_handler && master &&
                 ((master != m_is_master) ||
-                 (m_number_of_neighbor !=
-                  this->m_sys.body().sys().nodelist().size()))) {
+                 (m_number_of_neighbor <
+                      this->m_sys.body().sys().nodelist().size() &&
+                  this->m_sys.body().sys().nodelist().size() > 0))) {
               for (auto &topic_dmesgpb : m_topic_last_dmesgpb) {
                 dmn::DMesgPb pb = topic_dmesgpb.second;
 
