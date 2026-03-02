e721f552542dbb26d36476284339a83295adf6b2
Chee Bin Hoh <cheebinhoh@gmail.com>
2026-03-01 20:18:43 -0500

    improve

--- include/dmn-runtime.hpp
+++ include/dmn-runtime.hpp
@@ -346,14 +346,14 @@ void Dmn_Runtime_Manager::addJob(F &&fnc, Dmn_Runtime_Job::Priority priority,
   if constexpr (std::is_invocable_r_v<Dmn_Runtime_Task, F,
                                       const Dmn_Runtime_Job &>) {
     // User gave us a coroutine: use it directly
-    taskFnc = std::forward<F>(fnc);
+    taskFnc = std::move(std::forward<F>(fnc));
   } else {
     // User gave us a void function: wrap it
-    taskFnc = [f = std::forward<F>(fnc)](
-                  const Dmn_Runtime_Job &j) -> Dmn_Runtime_Task {
+    taskFnc = std::move([f = std::forward<F>(fnc)](
+                            const Dmn_Runtime_Job &j) -> Dmn_Runtime_Task {
       f(j);
       co_return;
-    };
+    });
   }
 
   Dmn_Runtime_Job rjob{.m_priority = priority,
@@ -397,14 +397,14 @@ void Dmn_Runtime_Manager::addTimedJob(
   if constexpr (std::is_invocable_r_v<Dmn_Runtime_Task, F,
                                       const Dmn_Runtime_Job &>) {
     // User gave us a coroutine: use it directly
-    taskFnc = std::forward<F>(fnc);
+    taskFnc = std::move(std::forward<F>(fnc));
   } else {
     // User gave us a void function: wrap it
-    taskFnc = [f = std::forward<F>(fnc)](
-                  const Dmn_Runtime_Job &j) -> Dmn_Runtime_Task {
+    taskFnc = std::move([f = std::forward<F>(fnc)](
+                            const Dmn_Runtime_Job &j) -> Dmn_Runtime_Task {
       f(j);
       co_return;
-    };
+    });
   }
 
   Dmn_Runtime_Job rjob{.m_priority = priority,
