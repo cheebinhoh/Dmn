726e51e4ada007a9c59e515af9034c994fb56d09
Chee Bin Hoh <cheebinhoh@gmail.com>
2026-03-22 16:41:56 -0400

    refine the Dmn_Io api

--- include/dmn-io.hpp
+++ include/dmn-io.hpp
@@ -21,9 +21,6 @@
  *    not take ownership of the provided object; implementations SHOULD copy the
  *    value if they need to retain or change it.
  *
- *  - write(T &item): Takes an lvalue reference and indirect to the
- *    write(const T &item).
- *
  *  - write(T &&item): Takes an rvalue reference. Implementations SHOULD move
  *    from the item when possible to avoid unnecessary copies.
  *
@@ -91,16 +88,6 @@ public:
    * @param item The item to write.
    */
   virtual void write(const T &item) = 0;
-
-  /**
-   * @brief Write (copy) an item to the sink.
-   *
-   * The const lvalue overload; implementations SHOULD copy @p item if they
-   * need to retain it beyond the call.
-   *
-   * @param item The item to write.
-   */
-  virtual void write(T &item) final { write(std::as_const(item)); }
 
   /**
    * @brief Write (move) an item to the sink.
